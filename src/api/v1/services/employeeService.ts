675506c3df651896ef8c26705c362fe39adebf2c
OM J PATEL <opatel@academic.rrc.ca>
2025-01-30 15:11:37 -0600

    Added the get all employee export.

--- src/api/v1/services/employeeService.ts
+++ src/api/v1/services/employeeService.ts
@@ -1,14 +1,14 @@
 import { Employee } from "../interfaces/employee";
-import { v4 as uuidv4 } from "uuid";
 
-const employees: Employee[] = [];
+// Mock in-memory "database"
+let employees: Employee[] = [];
 
-export const createEmployee = (data: Omit<Employee, "id">): Employee => {
-  const newEmployee: Employee = { id: uuidv4(), ...data };
+export const createEmployee = async (employeeData: Employee): Promise<Employee> => {
+  const newEmployee = { id: employees.length + 1, ...employeeData };
   employees.push(newEmployee);
   return newEmployee;
 };
 
-export const getEmployees = (): Employee[] => {
+export const getAllEmployees = async (): Promise<Employee[]> => {
   return employees;
 };
