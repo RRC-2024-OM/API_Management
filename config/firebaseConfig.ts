68f5be6f7e04ef57bcce0d992e66d6562df718c6
OM J PATEL <opatel@academic.rrc.ca>
2025-02-13 17:41:03 -0600

    Integrate Firebase and Firestore.

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+import { getFirestore, Firestore } from "firebase-admin/firestore";
+import * as serviceAccount from "api-management-318bc-firebase-adminsdk-fbsvc-65d936c639.json"
+
+// Initialize the Firebase app with the service account credentials
+initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
+});
+
+// Get a reference to the Firestore service
+const db: Firestore = getFirestore();
+
+export { db };
