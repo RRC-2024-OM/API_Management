6b1cb9cdd5b65ce3412f9433b36e82285e5dc60a
OM J PATEL <opatel@academic.rrc.ca>
2025-02-17 22:54:58 -0600

    Refactor Firebase configuration to directly import service account credentials, improving clarity and reducing reliance on environment variables

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,10 +1,6 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-
-// Load Firebase credentials from an environment variable
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-    : require("../api-management-318bc-firebase-adminsdk-fbsvc-65d936c639.json");
+import * as serviceAccount from "api-management-318bc-firebase-adminsdk-fbsvc-65d936c639.json"
 
 // Initialize the Firebase app with the service account credentials
 initializeApp({
